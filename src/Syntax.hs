16ec79ec98bcd205d19ee8ccca5076fdbb0a351f
Nanaka Takano <btslovesg@gmail.com>
2022-12-13 18:20:49 +0900

    1208

--- src/Syntax.hs
+++ src/Syntax.hs
@@ -1,7 +1,9 @@
 module Syntax 
     where
 
+import Data.Char
 import Data.List
+import Text.ParserCombinators.ReadP
 
 data Expr
     = ENum Float
@@ -11,10 +13,17 @@ data Expr
     | EWhen Expr Expr
     | EIf Expr Expr Expr
     | EProcCall String [Expr]
-    deriving (Eq)
+    | EArith Bop Expr Expr
+    | EProcDef String [String]
+    deriving (Eq, Show)
 
-instance Show Expr where
-    show = showExpr
+isAtom :: Expr -> Bool
+isAtom (ENum _) = True
+isAtom (EVar _) = True
+isAtom _ = False
+
+-- instance Show Expr where
+--     show = showExpr
 
 showExpr :: Expr -> String
 showExpr e = case e of
@@ -25,6 +34,36 @@ showExpr e = case e of
     EWhen e1 e2 -> intercalate " " (["when"] ++ map showExpr [e1, e2])
     EIf e1 e2 e3 -> intercalate " " ["if", showExpr e1, "then", showExpr e2, "else", showExpr e3]
     EProcCall proc vs -> intercalate " " ([proc] ++ map showExpr vs)
+    EArith op e1 e2 -> unwords [show' e1, show op, show' e1]
+        where
+            show' e | isAtom e = show e
+                    | otherwise = "(" ++ show e ++ ")"
+    EProcDef proc vs -> unwords (proc : vs)
 
 sample1 :: Expr
 sample1 = ERepeat (ENum 4) (EBlock [EProcCall "fd" [ENum 200], EProcCall "lt" [ENum 90]])
+
+data Bop
+    = Add | Sub | Mul | Div deriving (Eq)
+
+instance Show Bop where
+    show Add = "+" 
+    show Sub = "-"
+    show Mul = "*"
+    show Div = "/"
+--
+
+instance Read Expr where
+    readsPrec _ = readP_to_S userInputR
+
+userInputR :: ReadP Expr
+userInputR = {- exprR +++ -} procdefR
+
+procdefR :: ReadP Expr
+procdefR = EProcDef <$> (string "proc" *> idenR) <*> many1 idenR
+
+idenR :: ReadP String
+idenR = skipSpaces *> munch1 isLetter
+
+exprR :: ReadP Expr
+exprR = undefined
